e625e65e8ee0d05900bdd5d2b2163f1cedd51893
Golden Eagle <joowoon.kang@biola.edu>
2020-07-11 21:21:35 -0700

    get by id endpoint is wokring

--- index.js
+++ index.js
@@ -19,7 +19,18 @@ app.get("/todos", async (req, res) => {
   }
 });
 //get a todo
+app.get("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const oneTodo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
 
+    res.json(oneTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 //create a todo
 app.post("/todos", async (req, res) => {
   try {
