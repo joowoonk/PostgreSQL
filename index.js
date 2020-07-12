49e5c29670c3de454da85745ab3e8f38c7417dd1
Golden Eagle <joowoon.kang@biola.edu>
2020-07-11 21:35:47 -0700

    delete and update are working

--- index.js
+++ index.js
@@ -49,8 +49,36 @@ app.post("/todos", async (req, res) => {
 });
 
 //update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params; //where
+    const { description } = req.body; //SET
+
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json({ id, description });
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 //detele a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    res.json("Todo was successfully deleted!");
+  } catch (err) {
+    console.err(err.message);
+  }
+});
+
+//server running
 
 app.listen(5000, () => {
   console.log("server is listening on port 5000");
